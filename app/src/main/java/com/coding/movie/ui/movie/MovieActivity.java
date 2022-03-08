ae75bf61e439ebac3b622e31cefbb1cfafc9a283
Nazanin <khazaee.nazanin.71@gmail.com>
2022-03-08 11:31:22 +0100

    add database favorite movies

--- app/src/main/java/com/coding/movie/ui/movie/MovieActivity.java
+++ app/src/main/java/com/coding/movie/ui/movie/MovieActivity.java
@@ -47,7 +47,8 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
     private FavDB favDB;
     private String imdbId = "";
     private String poster = "";
-    private Boolean favStatus = false;;
+    private Boolean favStatus = false;
+    private MovieFavItem favItem;
 
 
     @Override
@@ -63,18 +64,15 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
 
     @Override
     protected void init() {
+        movieName = getIntent().getStringExtra("Movie");
         btnAddFavorite.setEnabled(false);
         favDB = new FavDB(mContext);
         //create table on first
-        SharedPreferences prefs = mContext.getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        boolean firstStart = prefs.getBoolean("firstStart", true);
+        boolean firstStart = Hawk.get("firstStart", true);
         if (firstStart) {
             createTableOnFirstStart();
         }
-        movieName = getIntent().getStringExtra("Movie");
-        Log.i("name", movieName);
         presenter.search(movieName);
-
     }
 
     //#region click
@@ -85,24 +83,27 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
 
     @OnClick(R.id.btn_add_favorite)
     void setBtnAddFavorite() {
-        if (favStatus){
+        if (favStatus) {
             btnAddFavorite.setImageResource(R.drawable.ic_favorite_withe);
             favStatus = false;
             Hawk.delete(imdbId);
-        }else{
+            favDB.remove_fav(favItem.getKey_id());
+        } else {
             btnAddFavorite.setImageResource(R.drawable.ic_favorite_red);
             favStatus = true;
-            Hawk.put("MovieId",imdbId);
+            Hawk.put("MovieId", imdbId);
+            favDB.insertIntoTheDatabase(favItem.getTitle(), favItem.getImageResourse(),
+                    favItem.getKey_id(), favItem.getRating(), favItem.getDesc(), favItem.getFavStatus().toString());
         }
     }
 
     @Override
     public void onSearchResult(SearchMoviesWebModels searchMoviesWebModels) {
         btnAddFavorite.setEnabled(true);
-        if (searchMoviesWebModels.getImdbID()!= null){
+        if (searchMoviesWebModels.getImdbID() != null) {
             imdbId = searchMoviesWebModels.getImdbID();
         }
-        if (searchMoviesWebModels.getPoster()!= null){
+        if (searchMoviesWebModels.getPoster() != null) {
             poster = searchMoviesWebModels.getPoster();
         }
         if (poster.length() > 10) {
@@ -110,10 +111,13 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
                     .load(searchMoviesWebModels.getPoster())
                     .into(ivMoviePoster);
         }
-        tvMovieTitle.setText(searchMoviesWebModels.getTitle());
-        tvMovieRating.setText(searchMoviesWebModels.getImdbRating());
-        tvMovieDescription.setText(searchMoviesWebModels.getPlot());
-        String movieId = Hawk.get("MovieId","");
+        String title = searchMoviesWebModels.getTitle();
+        tvMovieTitle.setText(title);
+        String rating = searchMoviesWebModels.getImdbRating();
+        tvMovieRating.setText(rating);
+        String desc = searchMoviesWebModels.getPlot();
+        tvMovieDescription.setText(desc);
+        String movieId = Hawk.get("MovieId", "");
         if (movieId != null && movieId.equals(imdbId)) {
             btnAddFavorite.setImageResource(R.drawable.ic_favorite_red);
             favStatus = true;
@@ -122,6 +126,7 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
             favStatus = false;
         }
 
+        favItem = new MovieFavItem(poster, title, imdbId, favStatus, rating, desc);
     }
 
     @Override
@@ -162,116 +167,7 @@ public class MovieActivity extends BaseActivity implements MovieContract.View {
 
     private void createTableOnFirstStart() {
         favDB.insertEmpty();
-
-        SharedPreferences prefs = mContext.getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("firstStart", false);
-        editor.apply();
+        Hawk.put("firstStart", false);
     }
-
-//    private void readCursorData(MovieFavItem favItem, String title) {
-//        Cursor cursor = favDB.read_all_data(favItem.getKey_id());
-//        SQLiteDatabase db = favDB.getReadableDatabase();
-//        try {
-//            while (cursor.moveToNext()) {
-//                String item_fav_status = cursor.getString(cursor.getColumnIndex(FavDB.FAVORITE_STATUS));
-//                coffeeItem.setFavStatus(item_fav_status);
-//
-//                //check fav status
-//                if (item_fav_status != null && item_fav_status.equals("1")) {
-//                    viewHolder.favBtn.setBackgroundResource(R.drawable.ic_favorite_red_24dp);
-//                } else if (item_fav_status != null && item_fav_status.equals("0")) {
-//                    viewHolder.favBtn.setBackgroundResource(R.drawable.ic_favorite_shadow_24dp);
-//                }
-//            }
-//        } finally {
-//            if (cursor != null && cursor.isClosed())
-//                cursor.close();
-//            db.close();
-//        }
-//
-//    }
-
-//    // like click
-//    private void likeClick (MovieFavItem favItem, Button favBtn, final TextView textLike) {
-//        DatabaseReference refLike = FirebaseDatabase.getInstance().getReference().child("likes");
-//        final DatabaseReference upvotesRefLike = refLike.child(coffeeItem.getKey_id());
-//
-//        if (favItem.getFavStatus().equals("0")) {
-//
-//            favItem.setFavStatus("1");
-//
-//            favDB.insertIntoTheDatabase(favItem.getTitle(), favItem.getImageResourse(),
-//                    favItem.getKey_id(), favItem.getRating(),favItem.getDesc(), favItem.getFavStatus());
-//
-//            favBtn.setBackgroundResource(R.drawable.ic_favorite_red);
-//            favBtn.setSelected(true);
-//
-//            upvotesRefLike.runTransaction(new Transaction.Handler() {
-//                @NonNull
-//                @Override
-//                public Transaction.Result doTransaction(@NonNull final MutableData mutableData) {
-//                    try {
-//                        Integer currentValue = mutableData.getValue(Integer.class);
-//                        if (currentValue == null) {
-//                            mutableData.setValue(1);
-//                        } else {
-//                            mutableData.setValue(currentValue + 1);
-//                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    textLike.setText(mutableData.getValue().toString());
-//                                }
-//                            });
-//                        }
-//                    } catch (Exception e) {
-//                        throw e;
-//                    }
-//                    return Transaction.success(mutableData);
-//                }
-//
-//                @Override
-//                public void onComplete(@Nullable DatabaseError databaseError, boolean b, @Nullable DataSnapshot dataSnapshot) {
-//                    System.out.println("Transaction completed");
-//                }
-//            });
-//
-//
-//        } else if (favItem.getFavStatus().equals("1")) {
-//            favItem.setFavStatus("0");
-//            favDB.remove_fav(favItem.getKey_id());
-//            favBtn.setBackgroundResource(R.drawable.ic_favorite_shadow_24dp);
-//            favBtn.setSelected(false);
-//
-//            upvotesRefLike.runTransaction(new Transaction.Handler() {
-//                @NonNull
-//                @Override
-//                public Transaction.Result doTransaction(@NonNull final MutableData mutableData) {
-//                    try {
-//                        Integer currentValue = mutableData.getValue(Integer.class);
-//                        if (currentValue == null) {
-//                            mutableData.setValue(1);
-//                        } else {
-//                            mutableData.setValue(currentValue - 1);
-//                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    textLike.setText(mutableData.getValue().toString());
-//                                }
-//                            });
-//                        }
-//                    } catch (Exception e) {
-//                        throw e;
-//                    }
-//                    return Transaction.success(mutableData);
-//                }
-//
-//                @Override
-//                public void onComplete(@Nullable DatabaseError databaseError, boolean b, @Nullable DataSnapshot dataSnapshot) {
-//                    System.out.println("Transaction completed");
-//                }
-//            });
-//        }
-//    }
 
 }
